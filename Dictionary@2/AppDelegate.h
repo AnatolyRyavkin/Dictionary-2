1ec2adbc377f6dc6aa2b0157dcaafdf6abe9719e
AnatolyRyavkin <toryavkin@gmail.com>
2019-09-29 16:06:40 +0300

    Initial Commit

---
+++ Dictionary@2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Dictionary@2
+//
+//  Created by Anatoly Ryavkin on 29/09/2019.
+//  Copyright © 2019 AnatolyRyavkin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Dictionary@2/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Dictionary@2
+//
+//  Created by Anatoly Ryavkin on 29/09/2019.
+//  Copyright © 2019 AnatolyRyavkin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
